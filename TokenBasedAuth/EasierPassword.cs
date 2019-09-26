f6725aa54514393917eca8146abb775c0ca68c1c
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-09-26 08:22:48 +0300

    Create EasierPassword.cs

---
+++ TokenBasedAuth/EasierPassword.cs
@@ -0,0 +1,10 @@
+            // SNIPPET -- IdentityConfig.cs
+            // Configure validation logic for passwords
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
+            };
